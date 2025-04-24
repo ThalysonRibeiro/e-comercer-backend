fc5a17270e18c52d6403833f8dc95ea75ed44113
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-04-24 09:43:24 -0300

    feat: route public

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -12,10 +12,11 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { AccountType } from '@prisma/client';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(private readonly reviewService: ReviewService) { }
 
   @Post()
   create(@Body() createReviewDto: CreateReviewDto) {
@@ -26,11 +27,14 @@ export class ReviewController {
   findAllReviewUser(@Param('id') id: string) {
     return this.reviewService.findAllReviewUser(id);
   }
+
+  @Public()
   @Get('product/:id')
   findAllReviewProduct(@Param('id') id: string) {
     return this.reviewService.findAllReviewProduct(id);
   }
 
+  @Public()
   @Get()
   findAllReview() {
     return this.reviewService.findAllReview();
