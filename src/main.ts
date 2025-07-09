855a9682ece06932b7168a9f5b956f1bbf500a7d
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-07-09 17:34:54 -0300

    feat: add config cors

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,28 @@ async function bootstrap() {
 
   const reflector = app.get(Reflector);
   const configService = app.get(ConfigService);
+
   app.useGlobalGuards(new ApiKeyGuard(reflector, configService));
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      configService.get('SITE_URL'),
+      configService.get('MOBILE_URL'),
+      configService.get('DASHBOARD_URL'),
+    ].filter(Boolean),
     methods: 'GET, POST, PUT, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization, x-api-key',
+    credentials: true, // Permite envio de cookies/auth headers
   });
 
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Habilita a transformação de tipos (de string para number, etc)
       whitelist: true, // Remove propriedades não declaradas no DTO
+      forbidNonWhitelisted: true, // Rejeita requisições com propriedades não permitidas
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3333);
+  await app.listen(configService.get('PORT') ?? 3333);
 }
 bootstrap();
